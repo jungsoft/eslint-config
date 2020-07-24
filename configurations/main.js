bcffc0631da802aded8c6e60ed96c73f0bc5e1a9
Laura <laurabeatriserafim@gmail.com>
2020-07-24 08:45:51 -0300

    Fix quotes

--- configurations/main.js
+++ configurations/main.js
@@ -1,25 +1,25 @@
 module.exports = {
-  parser: 'babel-eslint',
+  parser: "babel-eslint",
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module',
+    sourceType: "module",
   },
   env: {
     es6: true
   },
   plugins: [
-    'import'
+    "import"
   ],
   extends: [
     // ESLint Base Rules
-    '../rules/eslint/stylistic',
-    '../rules/eslint/variables',
-    '../rules/eslint/errors',
-    '../rules/eslint/es6',
-    '../rules/eslint/best-practices',
+    "../rules/eslint/stylistic",
+    "../rules/eslint/variables",
+    "../rules/eslint/errors",
+    "../rules/eslint/es6",
+    "../rules/eslint/best-practices",
 
     // Import Plugin Rules
-    '../rules/import/helpful-warnings',
-    '../rules/import/style-guide'
+    "../rules/import/helpful-warnings",
+    "../rules/import/style-guide"
   ].map(require.resolve)
 };
