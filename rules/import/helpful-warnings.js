f106b263d64b4673672f506f990dbd93ec368cd5
Laura <laurabeatriserafim@gmail.com>
2020-07-23 21:56:02 -0300

    Map import plugin rules

---
+++ rules/import/helpful-warnings.js
@@ -0,0 +1,25 @@
+module.exports = {
+  /**
+   * Import plugin helpful warning rules
+   * @see https://github.com/benmosher/eslint-plugin-import#rules
+   */
+  rules: {
+    /**
+     * Forbid the use of extraneous packages
+     * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-extraneous-dependencies.md
+     */
+    "import/no-extraneous-dependencies": ["error", { devDependencies: true }],
+
+     /**
+     * Report use of exported name as identifier of default export
+     * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default.md
+     */
+    "import/no-named-as-default": 0,
+
+    /**
+     * Report use of exported name as property of default export
+     * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default-member.md
+     */
+    "import/no-named-as-default-member": 0,
+  }
+}
---
+++ rules/import/style-guide.js
@@ -0,0 +1,26 @@
+module.exports = {
+  /**
+   * Import plugin style guide rules
+   * @see https://github.com/benmosher/eslint-plugin-import#rules
+   */
+  rules: {
+    /**
+     * Prefer a default export if module exports a single name
+     * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
+     */
+    "import/prefer-default-export": 0,
+
+    /**
+     * Enforce a convention in module import order
+     * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+     */
+    "import/order": ["error", {
+      "newlines-between": "always",
+      groups: [
+        ["external", "builtin"],
+        ["internal"],
+        ["parent", "sibling", "index"],
+      ],
+    }],
+  }
+}
