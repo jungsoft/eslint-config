51a3f0317fc4b99af41461a68d95204e7bc3ff54
Laura <laurabeatriserafim@gmail.com>
2020-07-25 22:41:09 -0300

    Remove rules object from react native config

--- configurations/react-native.js
+++ configurations/react-native.js
@@ -17,12 +17,5 @@ module.exports = {
   ],
   env: {
     "react-native-globals/all": true
-  },
-  rules: {
-    "react-native/no-inline-styles": 1,
-    "react-native/no-color-literals": 2,
-    "react-native/no-unused-styles": 2,
-    "react-native/no-inline-styles": 2,
-    "react-native/no-raw-text": 2,
   }
 };
