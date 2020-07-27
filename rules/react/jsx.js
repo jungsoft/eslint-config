9b32c44a425745dd0ad1edc5a772fffbbae37ec1
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2020-07-27 07:52:45 -0300

    Disable jsx-props-no-spreading rule

--- rules/react/jsx.js
+++ rules/react/jsx.js
@@ -11,5 +11,11 @@ module.exports = {
      * @see https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-filename-extension.md
      */
     "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
+    
+    /**
+     * Prevent JSX prop spreading
+     * @see https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-props-no-spreading.md
+     */
+    "react/jsx-props-no-spreading": 0,
   }
 }
